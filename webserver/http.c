187781de85f6920194e6d9cea516cd08099643b6
Edouard CATTEZ <teamexcalicraft@gmail.com>
2015-02-09 14:20:36 +0100

    Amelioration du simple get

--- webserver/http.c
+++ webserver/http.c
@@ -2,63 +2,49 @@
 
 int verifier_entete(char * request)
 {	
-	const int NB_SPACES = 3;
+	const int NB_WORDS = 3;
 	char *tmp;
 	char **res;
 	char *split;
-	int spaces;
+	int result;
+	int words;
 	
 	tmp = request;
 	res = NULL;
 	split = strtok(tmp, " ");
-	spaces = 0;
+	words = 0;
+	result = 0;
 	
 	/* On découpe la requête et on stocke les parties dans res */
 	while (split)
 	{
-		++spaces;
-		res = realloc(res, sizeof(char*) * spaces);	
+		++words;
+		res = realloc(res, sizeof(char*) * words);
 		
 		if (res == NULL)
 		{
 			return -1;
 		}
-		res[spaces-1] = split;
+		res[words-1] = split;
 		split = strtok(NULL, " ");
 	}
 	
-	/* On ajoute en dernière partie du split une valeur pour indiquer la fin de la découpe */
-	res = realloc(res, sizeof(char*) * (spaces+1));
-	if (res == NULL)
-	{
-		return -1;
-	}
-	res[spaces] = 0;
-	
 	/* La requête doit contenir exactement 3 mots */
-	if (spaces != NB_SPACES)
-	{
-		return -1;
-	}
-	
 	/* La requête doit commencer par GET */
-	if (strcmp("GET", res[0]) != 0)
-	{
-		return -1;
-	}
-	
 	/* Vérifier que le 3ème mot est de la forme HTTP/M.m (M = 1, m = 0 ou 1) */
-	if (strncmp("HTTP/1.0", res[2], 8) != 0 && strncmp("HTTP/1.1", res[2], 8) != 0)
+	if (words != NB_WORDS || strcmp("GET", res[0]) != 0 || (strncmp("HTTP/1.0", res[2], 8) != 0 && strncmp("HTTP/1.1", res[2], 8) != 0))
 	{
-		return -1;
+		result = -1;
 	}
 	
-	return 0;
+	free(res);
+	return result;
 }
 
 void bad_request_400(FILE * stream)
 {
-	fprintf(stream, "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad request\r\n");
+	const char *error = "400 Bad request\r\n";
+	fprintf(stream, "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: %d\r\n\r\n%s", (int)strlen(error), error);
 	fflush(stream);
 }
 
